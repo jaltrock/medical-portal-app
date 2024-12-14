36f06928d047a9b62001a4f53192ca450ba708f2
jaltrock <altrockweb@gmail.com>
2024-12-13 19:21:45 -0500

    Added Header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <img src="/logo.svg" className="App-logo" alt="logo" />
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/searchpatient">Search</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
