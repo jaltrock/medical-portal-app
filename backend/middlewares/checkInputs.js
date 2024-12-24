fdcff3432d5a472760ff91d4417b06ba02ba9e6d
jaltrock <altrockweb@gmail.com>
2024-12-24 12:05:59 -0500

    Added Register Doctor functionality

--- backend/middlewares/checkInputs.js
+++ backend/middlewares/checkInputs.js
@@ -1,47 +1,160 @@
 const returnStatus = require("../helpers/returnStatus");
 
+function checkIDNumber(req, res, next) {
+  const { idnumber } = req.body;
+  if (!idnumber) {
+    returnStatus(res, 400, true, "idnumber field is missing");
+    return next(new Error("idnumber field is missing"));
+  }
+
+  // Check if the idnumber only contains numbers and it MUST have 8 digits assuming the idnumber of a citizen has 8 digits
+  const idnumberRegex = /^\d{8}$/;
+
+  const result = idnumberRegex.test(idnumber);
+
+  if (!result) {
+    returnStatus(res, 400, true, "id number is invalid");
+    return next(new Error("id number is invalid"));
+  }
+  next();
+}
+
+function checkUserName(req, res, next) {
+  const { username } = req.body;
+
+  if (!username) {
+    returnStatus(res, 400, true, "username field is missing");
+    return next(new Error("username field is missing"));
+  }
+
+  // Check if the username only contains characters between 'a-z' or 'A-Z'
+  const usernameRegex = /^[a-zA-Z\s]+$/;
+
+  // Check if the length of the username is within the limit 50 characters
+  if (username.length > 50) {
+    returnStatus(res, 400, true, "username too long");
+    return next(new Error("username too long"));
+  }
+
+  // Check if the username matches the regular expression
+  const result = usernameRegex.test(username);
+
+  if (!result) {
+    returnStatus(res, 400, true, "username is invalid");
+    return next(new Error("username is invalid"));
+  }
+
+  //   If this line is reached then continue,everything is ok
+  next();
+}
+
 function checkPassword(req, res, next) {
+  const { password } = req.body;
 
-    const { password } = req.body;
+  if (!password) {
+    returnStatus(res, 400, true, "password is misssing");
+    return next(new Error("passsword is missing"));
+  }
 
-    if(!password) {
-        returnStatus (res, 400, true, "Password is missing");
-        return next(new Error("Password is missing"));
-    }
+  if (password.length > 20) {
+    returnStatus(res, 400, true, "Password too long,max 20 characters allowed");
+    return next(new Error("Password too long,max 20 characters allowed"));
+  }
 
-    if(password.length > 20) {
-        returnStatus(res, 400, true, "Password too long, max 20 characters allowed");
-        return next(new Error("Password too long, max 20 characters allowed"));
-    }
-    // If this line is reached then continue
-    next();
+  //   If this line is reached then continue,everything is ok
+  next();
 }
 
 function checkEmail(req, res, next) {
-    const { email } = req.body;
+  const { email } = req.body;
 
-    if(!email) {
-        returnStatus(res, 400, true, "Email is missing");
-        return next(new Error("Email is missing"));
-    }
+  if (!email) {
+    returnStatus(res, 400, true, "Email is missing");
+    return next(new Error("Email is missing"));
+  }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // Check that the total length of the email address does not exceed 50 characters
-    if(email.length > 50) {
-        returnStatus(res, 400, true, "Email too long");
-        return next(new Error("Email too long"));
-    }
-    // This will either return true or false
-    const result = emailRegex.test(email);
-    if(!result) {
-        returnStatus( res, 400, true, "Email is invalid");
-        return next(new Error("Email is invalid"));
-    }
-    // If this line is reached then continue
-    next();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Check if the total length of the email address does not exceed 50 characters
+  if (email.length > 50) {
+    returnStatus(res, 400, true, "Email too long");
+    return next(new Error("Email too long"));
+  }
+  // This will either return True or False
+  const result = emailRegex.test(email);
+
+  if (!result) {
+    returnStatus(res, 400, true, "Email is invalid");
+    return next(new Error("Email is invalid"));
+  }
+
+  //   If this line is reached then continue,everything is ok
+  next();
+}
+
+function checkAddress(req, res, next) {
+  const { address } = req.body;
+
+  if (!address) {
+    returnStatus(res, 400, true, "Address is missing");
+    return next(new Error("Address is missing"));
+  }
+
+  if (address.length > 100) {
+    returnStatus(res, 400, true, "address too long");
+    return next(new Error("address too long"));
+  }
+
+  // allow .,/'-
+  const addressRegex = /^[a-zA-Z0-9\s.,\/'-]+$/;
+
+  const result = addressRegex.test(address);
+
+  if (!result) {
+    returnStatus(res, 400, true, "Address is invalid");
+    return next(new Error("Address is invalid"));
+  }
+
+  next();
+}
+
+function checkPhoneNumber(req, res, next) {
+  const { phone } = req.body;
+
+  if (!phone) {
+    returnStatus(res, 400, true, "Phone is missing");
+    return next(new Error("Phone is missing"));
+  }
+
+  if (phone.length > 17) {
+    returnStatus(res, 400, true, "phone too long");
+    return next(new Error("phone too long"));
+  }
+  /*
+  These kind of numbers will be valid:
+      123-456-7890
+      +1
+      (123) 456-7890 
+      123 456 7890
+      +1 (123) 456-7890
+      +123456789012345
+      123456789012345
+  */
+  const phoneRegex = /^[0-9 ()+-]+$/;
+  const result = phoneRegex.test(phone);
+
+  if (!result) {
+    returnStatus(res, 400, true, "Phone is invalid");
+    return next(new Error("Phone is invalid"));
+  }
+
+  next();
 }
 
 module.exports = {
-    checkPassword: checkPassword,
-    checkEmail: checkEmail,
+  checkIDNumber: checkIDNumber,
+  checkUserName: checkUserName,
+  checkPassword: checkPassword,
+  checkEmail: checkEmail,
+  checkAddress: checkAddress,
+  checkPhoneNumber: checkPhoneNumber,
 };
--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,40 +1,62 @@
-//import axios from "axios";
+import axios from "axios";
 
-export const makeGETrequest = async(url, token = "") => {
-    try{
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-            },
-        });
-        
-        const data = await response.json();
-        return data;
-    } catch(error) {
-        console.log("Error fetching data:", error);
-        return error;
-    }
+export const makeGETrequest = async (url, token = "") => {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    // response.json() method parses the JSON data from the response body of a Fetch API request and converts it into a JavaScript object.
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    return error;
+  }
 };
 
-export const makePOSTrequest = async (url, data, token="") => {
+export const makePOSTrequest = async (url, data, token = "") => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
+    const responseData = await response.json();
+    console.log(responseData);
+    return responseData;
+  } catch (error) {
+    console.error("Error posting data:", data);
+    return error;
+  }
+};
 
-        const responseData = await response.json()
-        console.log(responseData);
-        return responseData;
-    } catch(error) {
-        console.log("Error posting data:", data);
-        return error;
-    }
+
+export const makePOSTrequestForMultipleFormData = async (
+  url,
+  formData,
+  token = ""
+) => {
+  try {
+    // axios returns a data object on the response and we extract it using destructuring syntax below and data gives us data coming from backend
+    const { data } = await axios.post(url, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error posting data:", error);
+    // error.response.data gives us the data from the backend
+    return error.response.data;
+  }
 };
