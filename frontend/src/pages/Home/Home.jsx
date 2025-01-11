0a895b9e9ddd368c81fdad8a9976bd79643df9fd
jaltrock <altrockweb@gmail.com>
2025-01-10 21:50:25 -0500

    Updated home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,23 +1,35 @@
+import { useSelector } from "react-redux";
 import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <h2>Welcome to Patient Management System</h2>
-            <div className="featuers">
-                <div className="feature">
-                    <h3>Patient Registration</h3>
-                    <p>Register new patients into the system</p>
-                    <a href="/registerpatient">Register Patient</a>
-                </div>
-                <div className="feature">
-                    <h3>Search Patients</h3>
-                    <p>View and manage patients' medical history</p>
-                    <a href="/searchpatient">View Medical History</a>
-                </div>
-            </div>
+  const userSelector = useSelector((state) => state.user);
+  return (
+    <div className="home-container">
+      <h2>Welcome to Patient Management System</h2>
+      <h3>
+        {userSelector.username ? (
+          <p>
+            Welcome
+            <span style={{ color: "red" }}> {userSelector.username}</span>
+          </p>
+        ) : (
+          <p>You are not logged in</p>
+        )}
+      </h3>
+      <div className="features">
+        <div className="feature">
+          <h3>Patient Registration</h3>
+          <p>Register new patients into the system</p>
+          <a href="/registerpatient">Register Patient</a>
         </div>
-    );
+        <div className="feature">
+          <h3>Search Patients</h3>
+          <p>View and manage patients' medical history</p>
+          <a href="/searchpatient">View Medical History</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
